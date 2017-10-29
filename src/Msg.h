e06e9bd6c13bdbabb79a0f8d16d8cbc2323959b1
Cheng Fei <cheng.fei1026@gmail.com>
2017-10-29 15:20:03 +0800

    Update Msg.h

--- src/Msg.h
+++ src/Msg.h
@@ -1,4 +1,28 @@
 #ifndef _MSG_H_
 #define _MSG_H_
+typedef struct MsgHeader
+{
+  unsigned int id;
+} MsgHeader;
+
+typedef struct Msg
+{
+  MsgHeader header;
+  int payload;
+} Msg;
+
+template <typename Payload, unsigned id = 0>
+class CMsg
+{
+public:
+  const static ID = id;
+  CMsg(int size);
+  CMsg(void *msg);
+  ~CMsg();
+  void send(void);
+protected:
+  Payload *payLoad;
+  void *msg;
+};
 
 #endif
